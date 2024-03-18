338b5227c85d89b642f0209923ebd7f36b701f4b
Mason Zhang <1076536974@qq.com>
2024-03-18 16:16:36 +0800

    Update main.js
    实现defineProxy的响应式版本

--- main.js
+++ main.js
@@ -64,19 +64,37 @@ class MyVue{
       bindModel.push({ modelName: element.model, viewId: id, value: data[element.model] });
       virtualTemplate.push({...element,id})
     }
-    // Proxy API 优化
+    // Proxy 版本写法
     const that = this
-    const state = new Proxy(data,{
-      get:function(target,key,receiver) {
-        const ret = Reflect.get(target, key, receiver);
-        return that.getVal(key);
-      },
-      set:function(target,key,value,receiver) {
-        const ret = Reflect.set(target, key, value, receiver)
-        that.update(key, value);
-        return ret;
-      }
-  })
+    // const state = new Proxy(data,{
+    //   get:function(target,key,receiver) {
+    //     const ret = Reflect.get(target, key, receiver);
+    //     return that.getVal(key);
+    //   },
+    //   set:function(target,key,value,receiver) {
+    //     const ret = Reflect.set(target, key, value, receiver)
+    //     that.update(key, value);
+    //     return ret;
+    //   }
+    // })
+
+    // defineProxy版本写法
+    let state = {}
+    Object.keys(data).forEach((key) => {
+      Object.defineProperty(state, key, {
+        //但是默认是不可枚举的(for in打印打印不出来)，可：enumerable: true
+        //默认不可以修改，可：wirtable：true
+        //默认不可以删除，可：configurable：true
+        get: function () {
+          return that.getVal(key);
+        },
+        set: function (val) {
+          personName = that.update(key, val);
+        }
+      })
+    })
+    
+
     return {bindModel,virtualTemplate,state}
   };
   update(key, val) {
